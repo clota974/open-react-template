b6f20cefca8419d9b159ad6b28402a11f6d2f1e0
Killian Clotagatide <killian.clotagatide@pixelmatic.com>
2023-07-24 15:16:51 +0200

    phrase correction

--- components/hero.tsx
+++ components/hero.tsx
@@ -31,7 +31,7 @@ export default function Hero() {
 
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
-            <h1 className="h1 mb-4" data-aos="fade-up">Devez un Snack Bar connecté et innovant</h1>
+            <h1 className="h1 mb-4" data-aos="fade-up">Devenez le Snack Bar connecté et innovant</h1>
             <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">Notre système connecté centré sur le client permet aux Snack Bars réunionnais de réaliser des économies de temps et une fidélisation du client.</p>
             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
               <div data-aos="fade-up" data-aos-delay="600">
